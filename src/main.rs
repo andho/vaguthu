4bbead11f6c734d23c3b4f301003f9dc9e00438c
Andho Mohamed <andhos@gmail.com>
2024-10-04 16:52:40 +0500

    feat: use local timezone :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-extern crate timer;
 extern crate chrono;
+extern crate timer;
 
 use std::fs::File;
 use std::io::BufReader;
 use std::thread;
 
-use rodio::{Decoder, OutputStream, source::Source};
-use chrono::{Utc, Duration};
+use chrono::{Duration, Local};
+use rodio::{source::Source, Decoder, OutputStream};
 
 fn main() {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
@@ -14,10 +14,10 @@ fn main() {
     let timer = timer::Timer::new();
 
     let _guard = timer.schedule(
-        Utc::now() + Duration::seconds(0),
+        Local::now() + Duration::seconds(0),
         Some(Duration::minutes(20)),
         move || {
-            let dt = Utc::now();
+            let dt = Local::now();
             println!("{}: Play sound", dt.format("%Y-%m-%d %H:%M:%S"));
             let file = BufReader::new(File::open("assets/wrist-watch-alarm.mp3").unwrap());
             let source = Decoder::new(file).unwrap();
