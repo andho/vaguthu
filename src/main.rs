de4401cae494745b1520272cd183ccdda4a1becc
Andho Mohamed <andhos@gmail.com>
2024-11-10 15:26:35 +0500

    feat: set working time :sparkles:

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,27 @@
 extern crate chrono;
 extern crate timer;
 
-use std::fs::File;
+use std::{fs::File, str::FromStr};
 use std::io::BufReader;
 use std::thread;
 
-use chrono::{Duration, Local};
+use chrono::{Duration, Local, NaiveTime};
 use rodio::{source::Source, Decoder, OutputStream};
 
-fn main() {
+struct Schedule {
+    start: String,
+    end: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+
+    let schedule = Schedule {
+        start: "07:30".to_string(),
+        end: "22:00".to_string(),
+    };
+    let start_time = NaiveTime::from_str(&schedule.start)?;
+    let end_time = NaiveTime::from_str(&schedule.end)?;
 
     let timer = timer::Timer::new();
 
@@ -18,6 +30,25 @@ fn main() {
         Some(Duration::minutes(20)),
         move || {
             let dt = Local::now();
+            let start = dt.clone().with_time(start_time).unwrap();
+            let end = dt.clone().with_time(end_time).unwrap();
+
+            if dt.cmp(&start) == std::cmp::Ordering::Less {
+                println!(
+                    "{}: Too early to play sound",
+                    dt.format("%Y-%m-%d %H:%M:%S")
+                );
+                return;
+            }
+
+            if dt.cmp(&end) == std::cmp::Ordering::Greater {
+                println!(
+                    "{}: Too late to play sound",
+                    dt.format("%Y-%m-%d %H:%M:%S")
+                );
+                return;
+            }
+
             println!("{}: Play sound", dt.format("%Y-%m-%d %H:%M:%S"));
             let file = BufReader::new(File::open("assets/wrist-watch-alarm.mp3").unwrap());
             let source = Decoder::new(file).unwrap();
@@ -26,6 +57,6 @@ fn main() {
     );
 
     loop {
-        thread::sleep(std::time::Duration::new(1, 0));
+        thread::sleep(std::time::Duration::new(60, 0));
     }
 }
